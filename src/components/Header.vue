718e30e93d1135745be0832e96d131a3e917d230
Alex <alex.guzey@gmail.com>
2018-06-15 13:19:12 +0300

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar 
+        class="primary toolbar" 
+        dark app dense prominent
+    >
+        <v-toolbar-title>
+            <span class="toolbar-title__text">Regions charts</span>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
+
